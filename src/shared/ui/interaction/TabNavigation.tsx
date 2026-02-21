54ac028b16fa1c3d2feb5fdc77f7235367f26585
Asal Coding <aahmd.fauzii@gmail.com>
2026-02-22 04:43:45 +0700

    style(tab-navigation): show border only on hover/active

--- src/shared/ui/interaction/TabNavigation.tsx
+++ src/shared/ui/interaction/TabNavigation.tsx
@@ -140,8 +140,8 @@ const TabNavigationLink = React.forwardRef<
               )}
               <span
                 className={cx(
-                  "inline-flex items-center justify-center gap-xs rounded-md bg-surface-neutral-primary px-lg py-sm border border-neutral-primary shadow-none",
-                  "group-hover:bg-surface-state-neutral-light-hover",
+                  "inline-flex items-center justify-center gap-xs rounded-md bg-surface-neutral-primary px-lg py-sm border border-transparent shadow-none",
+                  "group-hover:bg-surface-state-neutral-light-hover group-hover:border-neutral-primary",
                   "group-data-[active]:border-foreground-brand-primary",
                 )}
               >
